ac6158b842b3ed053fb01739c7b57b64bbe7324e
Nimish Shinde <106652972+whynimoisalive@users.noreply.github.com>
2024-12-26 17:33:23 +0530

    hi

--- src/components/SearchInterface.jsx
+++ src/components/SearchInterface.jsx
@@ -153,7 +153,7 @@ const SearchInterface = (props) => {
                       className="flex-grow bg-transparent text-white outline-none mx-4 text-lg"
                       value={searchText}
                       onChange={(e) => setSearchText(e.target.value)}
-                      placeholder="Search Quantageddon"
+                      placeholder="Hint: Search Quantageddon"
                     />
                     <div className="flex items-center space-x-4">
                       <button type="button" className="hover:bg-[#3c4043] p-2 rounded-full transition-colors duration-200">
